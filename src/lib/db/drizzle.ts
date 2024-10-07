a70bfad7c5d512780f069338c337730ec0292521
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-07 23:24:28 +0200

    feat: create db client
    Signed-off-by: Aritra Mondal <aritra041999.dev@gmail.com>

---
+++ src/lib/db/drizzle.ts
@@ -0,0 +1,10 @@
+import { SECRET_TURSO_AUTH_TOKEN, SECRET_TURSO_DATABASE_URL } from '$env/static/private';
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/connect';
+
+export const db = await drizzle('turso', {
+	connection: {
+		url: SECRET_TURSO_DATABASE_URL!,
+		authToken: SECRET_TURSO_AUTH_TOKEN!
+	}
+});
