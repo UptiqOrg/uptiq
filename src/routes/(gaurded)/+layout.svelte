ed4f230e438f37043f01c5a3805c5584cd76b9de
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-08 03:08:25 +0200

    feat: add gaurded routes

---
+++ src/routes/(gaurded)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Navbar from '$lib/components/ui/navbar/navbar.svelte';
+</script>
+
+<section class="h-screen w-screen">
+	<Navbar />
+	<main class="border-t">
+		<div class="container mx-auto">
+			<slot />
+		</div>
+	</main>
+</section>
