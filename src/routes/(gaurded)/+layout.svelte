46a69c218346e3edbbb4fb0776a68599dc17e3ee
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-08 07:26:05 +0200

    chore: add meta store to keep track of window

--- src/routes/(gaurded)/+layout.svelte
+++ src/routes/(gaurded)/+layout.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
+	import { metaStore } from '$lib/store/meta.store';
 	import Navbar from '$lib/components/ui/navbar/navbar.svelte';
+	import SideBar from '$lib/components/ui/sidebar/container.svelte';
 </script>
 
-<section class="h-screen w-screen">
-	<Navbar />
-	<main class="border-t">
-		<div class="container mx-auto">
+<Navbar />
+<main
+	class={cn(
+		'fancy-background h-[calc(100vh-5rem)] w-screen overflow-hidden border-t',
+		$metaStore.isSmallScreen ? '' : 'flex'
+	)}
+>
+	<div class="container mx-auto flex">
+		<SideBar />
+		<div class="p-4">
 			<slot />
 		</div>
-	</main>
-</section>
+	</div>
+</main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script>
 	import '../app.css';
 	import '../font.css';
+	import { metaStore } from '$lib/store/meta.store';
+
+	let innerWidth = 0;
+
+	$: metaStore.set({
+		isSmallScreen: innerWidth < 1000,
+		showDashboardSidebar: false
+	});
 </script>
+
+<svelte:window bind:innerWidth />
 
 <slot />
