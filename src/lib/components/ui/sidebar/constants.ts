301db6cb1b591c21dff97d68e7819c2fd440d5ba
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-15 08:43:50 +0200

    feat: switch to organic routes and add items

--- src/lib/components/ui/sidebar/constants.ts
+++ src/lib/components/ui/sidebar/constants.ts
@@ -1,15 +1,27 @@
+import Users from 'lucide-svelte/icons/users';
+import ChartLine from 'lucide-svelte/icons/chart-line';
 import LayoutDashboard from 'lucide-svelte/icons/layout-dashboard';
 import WalletCards from 'lucide-svelte/icons/wallet-cards';
 
 export const navLinks = [
 	{
 		title: 'Dashboard',
-		href: 'dashboard',
+		href: '/dashboard',
 		icon: LayoutDashboard
 	},
 	{
 		title: 'Projects',
-		href: 'projects',
+		href: '/projects',
 		icon: WalletCards
+	},
+	{
+		title: 'Analytics',
+		href: '/analytics',
+		icon: ChartLine
+	},
+	{
+		title: 'Subscribers',
+		href: '/subscribers',
+		icon: Users
 	}
 ];
