247f3561ccc7fb1e56c24910d3da8eb7ade3b059
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-15 08:44:31 +0200

    chore: update navbar styling

--- src/lib/components/ui/navbar/navbar.svelte
+++ src/lib/components/ui/navbar/navbar.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Logo from '$lib/components/ui/logo/logo.svelte';
 	import Auth from '$lib/components/ui/auth/auth.svelte';
+	import { page } from '$app/stores';
+	import { cn } from '$lib/utils';
 </script>
 
 <nav class="flex h-20 w-screen place-items-center text-lg font-medium">
 	<div class="container mx-auto flex items-center justify-between">
-		<a href="/">
+		<a href="/" class={cn($page.url.pathname === '/' ? '' : 'ml-10 md:ml-0')}>
 			<Logo />
 		</a>
 		<Auth />
