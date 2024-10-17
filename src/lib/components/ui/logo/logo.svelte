c8bc56d6b09b890b63f9904dd1657024995fdb39
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-17 21:08:16 +0200

    feat: cleanup logo component

--- src/lib/components/ui/logo/logo.svelte
+++ src/lib/components/ui/logo/logo.svelte
@@ -2,6 +2,6 @@
 	import { Tally4 } from 'lucide-svelte';
 </script>
 
-<div class="flex w-fit items-center rounded-full bg-secondary px-6 py-2 text-lg font-medium">
+<div class="flex h-12 w-fit items-center rounded-full bg-background px-6 text-lg font-medium">
 	<Tally4 strokeWidth="3" class="mr-3.5 h-6 w-6 text-indigo-700" /> upTiq
 </div>
