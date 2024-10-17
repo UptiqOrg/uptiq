af452cd7977ea6e82785a0a353a08e852bd1969a
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-17 19:43:31 +0200

    chore: update logo component

--- src/lib/components/ui/logo/logo.svelte
+++ src/lib/components/ui/logo/logo.svelte
@@ -2,6 +2,6 @@
 	import { Tally4 } from 'lucide-svelte';
 </script>
 
-<span class="flex items-center text-lg font-medium">
-	<Tally4 strokeWidth="3" class="mr-2 h-6 w-6 text-indigo-700" /> upTiq
-</span>
+<div class="flex w-fit items-center rounded-full bg-secondary px-6 py-2 text-lg font-medium">
+	<Tally4 strokeWidth="3" class="mr-3.5 h-6 w-6 text-indigo-700" /> upTiq
+</div>
