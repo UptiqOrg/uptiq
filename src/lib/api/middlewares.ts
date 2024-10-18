1db01647d681e5e09c70aad81f676d934a12423c
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-18 13:41:52 +0200

    feat: add request body validation middleware

---
+++ src/lib/api/middlewares.ts
@@ -0,0 +1,25 @@
+import type { Context, Next } from 'hono';
+import { z, ZodError } from 'zod';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const validateRequestBody = <T extends z.ZodType<any, any>>(schema: T) => {
+	return async (context: Context, next: Next) => {
+		try {
+			const body = await context.req.json();
+			const valid = schema.safeParse(body);
+
+			if (valid.error) {
+				const errorResponse = {
+					error: 'Invalid input',
+					details: valid.error.errors.map((issue) => `${issue.path.join('.')} is ${issue.message}`)
+				};
+				return context.json(errorResponse, 400);
+			}
+
+			context.set('requestBody', valid.data as z.infer<T>);
+			await next();
+		} catch (error) {
+			return context.json({ error: 'Invalid input json' }, 400);
+		}
+	};
+};
