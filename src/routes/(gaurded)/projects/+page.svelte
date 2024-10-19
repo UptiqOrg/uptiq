a5edd4baac115da1b302b73fee1a34dc47fb6e35
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-19 13:56:58 +0530

    feat: store projects in project store

--- src/routes/(gaurded)/projects/+page.svelte
+++ src/routes/(gaurded)/projects/+page.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import * as Dialog from '$lib/components/ui/dialog';
+	import { onMount } from 'svelte';
+	import { projectStore, selectedProjectIdStore } from '$lib/store/project.store';
 	import { Button } from '$lib/components/ui/button';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import { get } from 'svelte/store';
 	import Header from '$lib/components/ui/dashboard/header.svelte';
 	import ProjectCard from '$lib/components/ui/project/project-card.svelte';
 	import ProjectForm from '$lib/components/ui/project/project-form.svelte';
-	import type { SelectProjectPartial } from '$lib/db/schema.js';
+	import ProjectDelete from '$lib/components/ui/project/project-delete.svelte';
 
 	export let data;
 
-	let selectedProject: SelectProjectPartial | null = null;
 	let showProjectFormDialog = false;
+	let showDeleteProjectDialog = false;
 
-	const updateProjectsList = (selected: SelectProjectPartial | null) => {
-		if (!selected) return;
-		let project = data.projects.find((project) => project.id === selected.id);
-		console.log(project);
-	};
-
-	$: updateProjectsList(selectedProject);
+	onMount(() => {
+		if (!data.projects) return null;
+		projectStore.set(data.projects);
+	});
 </script>
 
 <section class="flex items-center justify-between">
@@ -25,23 +25,35 @@
 	<Button
 		on:click={() => {
 			showProjectFormDialog = true;
-			selectedProject = null;
+			selectedProjectIdStore.set(null);
 		}}
 	>
 		Add Project
 	</Button>
 </section>
 <div class="w-full columns-xs gap-4">
-	{#each data.projects as project}
-		<ProjectCard {project} bind:showProjectFormDialog bind:selectedProject />
-	{/each}
+	{#if $projectStore && $projectStore.size > 0}
+		{#each $projectStore.keys() as projectId}
+			<ProjectCard {projectId} bind:showProjectFormDialog bind:showDeleteProjectDialog />
+		{/each}
+	{:else}
+		Loading projects ...
+	{/if}
 </div>
 
 <Dialog.Root bind:open={showProjectFormDialog}>
 	<Dialog.Content>
-		<Dialog.Title>Add Project</Dialog.Title>
+		<Dialog.Title>{get(selectedProjectIdStore) ? 'Update' : 'Add'} Project</Dialog.Title>
 		<Dialog.Description>
-			<ProjectForm bind:selectedProject bind:showProjectFormDialog />
+			<ProjectForm bind:showProjectFormDialog />
+		</Dialog.Description>
+	</Dialog.Content>
+</Dialog.Root>
+<Dialog.Root bind:open={showDeleteProjectDialog}>
+	<Dialog.Content>
+		<Dialog.Title>Delete Project</Dialog.Title>
+		<Dialog.Description>
+			<ProjectDelete bind:showDeleteProjectDialog />
 		</Dialog.Description>
 	</Dialog.Content>
 </Dialog.Root>
