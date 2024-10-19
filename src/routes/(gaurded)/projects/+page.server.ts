d130bcc052deeed385ab2581e453f7f48a7b766e
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-19 13:56:11 +0530

    feat: format project api response
    modify it to be a map of id, project

--- src/routes/(gaurded)/projects/+page.server.ts
+++ src/routes/(gaurded)/projects/+page.server.ts
@@ -2,11 +2,16 @@ import type { SelectProjectPartial } from '$lib/db/schema';
 import type { PageServerLoad } from '../../(auth)/signout/$types';
 
 export const load = (async ({ fetch }) => {
-	const projects: SelectProjectPartial[] = await fetch('/api/projects', {
+	const projects: Map<string, SelectProjectPartial> | null = await fetch('/api/projects', {
 		method: 'GET'
 	})
 		.then(async (response) => await response.json())
-		.then((response) => response.projects);
+		.then((response) => {
+			if (!response.projects) return null;
+			return new Map(
+				response.projects.map((project: SelectProjectPartial) => [String(project.id), project])
+			);
+		});
 
 	return { projects };
 }) satisfies PageServerLoad;
