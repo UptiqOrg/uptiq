422a41d66044056404a14ef4371193e6279ce798
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-22 18:51:59 +0530

    feat: thorw eror if no project is invalid

--- src/routes/(gaurded)/projects/[...slug]/+page.server.ts
+++ src/routes/(gaurded)/projects/[...slug]/+page.server.ts
@@ -1,10 +1,16 @@
 import type { SelectProject, SelectWebsitePartial } from '$lib/db/schema';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params, fetch }) => {
 	const project: SelectProject = await fetch(`/api/projects/${params.slug}`, {
 		method: 'GET'
-	}).then((response) => response.json());
+	})
+		.then((response) => response.json())
+		.then((project) => {
+			if (project.error) throw error(400, project.error);
+			return project;
+		});
 
 	const websites: Map<string, SelectWebsitePartial> = await fetch(`/api/websites/${params.slug}`, {
 		method: 'GET'
