7aa473693c89746e49a9e0a2f7eb28351b823099
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-22 22:37:16 +0530

    feat: add status page type

--- src/lib/api/types.ts
+++ src/lib/api/types.ts
@@ -1,4 +1,7 @@
+import type { SelectPartialStatus, SelectWebsitePartial } from '$lib/db/schema';
 import type { StatusCode } from 'hono/utils/http-status';
+
+export type StatusPageResponse = SelectWebsitePartial & { statuses: SelectPartialStatus[] };
 
 export type ServiceResponse<T> = {
 	data?: T;
