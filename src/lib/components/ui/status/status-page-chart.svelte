e3a92e9583cdbbced2a0ae0216f84754a7e99716
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-22 22:38:10 +0530

    feat: add status-page chart component

---
+++ src/lib/components/ui/status/status-page-chart.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { prettifyDate } from '$lib/utils';
+	import { statusColorMap } from './constants';
+	import * as HoverCard from '$lib/components/ui/hover-card';
+
+	export let statuses = [];
+</script>
+
+<div>
+	<div class="mx-auto mb-2 flex w-full items-center justify-center gap-0.5">
+		{#each statuses as status}
+			<HoverCard.Root openDelay={100} closeDelay={0}>
+				<HoverCard.Trigger
+					target="_blank"
+					rel="noreferrer noopener"
+					class="h-14 w-[2%] rounded-md"
+					style="background-color: {statusColorMap[status.status]}"
+				/>
+				<HoverCard.Content class="w-fit">
+					<ul class="text-sm text-muted-foreground">
+						<li class="flex justify-between">
+							Status: <span class="text-foreground">{status.status.toUpperCase()}</span>
+						</li>
+						<li class="flex justify-between">
+							Status Code: <span class="text-foreground">{status.statusCode}</span>
+						</li>
+						<li class="flex justify-between">
+							Response Time: <span class="ml-4 text-foreground">{status.responseTime}ms</span>
+						</li>
+					</ul>
+				</HoverCard.Content>
+			</HoverCard.Root>
+		{/each}
+	</div>
+	<div class="flex w-full items-center justify-between text-xs">
+		<div>{prettifyDate(new Date(statuses[0].createdAt))}</div>
+		<div>{prettifyDate(new Date(statuses[Math.ceil(statuses.length / 2)].createdAt))}</div>
+		<div>{prettifyDate(new Date(statuses[statuses.length - 1].createdAt))}</div>
+	</div>
+</div>
