33a38940803eab1fa6aab7fdc2ba7af56496ebc3
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-22 22:37:06 +0530

    feat: update status controller and service to return status page

--- src/lib/api/status/status.service.ts
+++ src/lib/api/status/status.service.ts
@@ -1,11 +1,18 @@
 import { db } from '$lib/db/drizzle';
-import { uptimeChecks, type SelectPartialStatus } from '$lib/db/schema';
+import {
+	projects,
+	uptimeChecks,
+	websites,
+	type SelectPartialStatus,
+	type SelectProjectPartial
+} from '$lib/db/schema';
 import { desc, eq } from 'drizzle-orm';
-import type { ServiceResponse } from '../types';
+import type { ServiceResponse, StatusPageResponse } from '../types';
 import type { StatusCode } from 'hono/utils/http-status';
 
 export const getStatus = async (
-	websiteId: number
+	websiteId: number,
+	limit = 40
 ): Promise<ServiceResponse<SelectPartialStatus[]>> => {
 	return await db
 		.select({
@@ -16,7 +23,7 @@ export const getStatus = async (
 		})
 		.from(uptimeChecks)
 		.where(eq(uptimeChecks.websiteId, websiteId))
-		.limit(40)
+		.limit(limit)
 		.orderBy(desc(uptimeChecks.createdAt))
 		.then((response) => {
 			return {
@@ -31,4 +38,73 @@ export const getStatus = async (
 				data: []
 			};
 		});
+};
+
+export const getStatusByProject = async (
+	projectSlug: string
+): Promise<ServiceResponse<SelectProjectPartial>> => {
+	return await db
+		.select({
+			id: projects.id,
+			name: projects.name,
+			description: projects.description,
+			slug: projects.slug
+		})
+		.from(projects)
+		.where(eq(projects.slug, projectSlug))
+		.limit(1)
+		.then((response) => {
+			if (response.length === 0) {
+				return {
+					status: 404 as StatusCode,
+					error: 'Project not found!'
+				};
+			}
+
+			return {
+				status: 200 as StatusCode,
+				data: response[0]
+			};
+		})
+		.catch((error) => {
+			console.error(error);
+			return {
+				status: 500 as StatusCode,
+				error: 'Error fetching project!'
+			};
+		});
+};
+
+export const getStatusPage = async (
+	projectId: number
+): Promise<ServiceResponse<StatusPageResponse[]>> => {
+	const response: StatusPageResponse[] = [];
+
+	const websitesResponse = await db
+		.select({
+			id: websites.id,
+			name: websites.name,
+			url: websites.url,
+			checkInterval: websites.checkInterval
+		})
+		.from(websites)
+		.where(eq(websites.projectId, projectId));
+
+	for (const website of websitesResponse) {
+		const statusResponse = await getStatus(website.id, 50);
+
+		if (statusResponse.status !== 200 || !statusResponse.data) {
+			return {
+				status: 500 as StatusCode,
+				error: 'Error fetching status'
+			};
+		}
+
+		response.push({
+			...website,
+			statuses: statusResponse.data
+		});
+	}
+
+	return { status: 200, data: response };
 };
