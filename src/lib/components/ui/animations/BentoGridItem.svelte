88cfdeafa23bd8729d5a02c7bc462b301609a73a
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-23 11:36:35 +0530

    feat: add bento grid item

---
+++ src/lib/components/ui/animations/BentoGridItem.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	export let className: string | undefined = undefined;
+</script>
+
+<div
+	class={cn(
+		'group/bento row-span-1 flex w-full flex-col justify-between space-y-4 rounded-xl border border-transparent bg-white px-6 py-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none',
+		className
+	)}
+>
+	<slot />
+</div>
