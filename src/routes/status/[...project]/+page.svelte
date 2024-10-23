1f5603e99a335f22ef328e2bd4c452f6e40cfb7c
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-23 11:40:09 +0530

    feat: move status card to seperate component

--- src/routes/status/[...project]/+page.svelte
+++ src/routes/status/[...project]/+page.svelte
@@ -2,10 +2,7 @@
 	import { onMount } from 'svelte';
 	import * as Card from '$lib/components/ui/card';
 	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
-	import StatusBadge from '$lib/components/ui/status/status-badge.svelte';
-	import StatusAvailability from '$lib/components/ui/status/status-availability.svelte';
-	import StatusPerformance from '$lib/components/ui/status/status-performance.svelte';
-	import StatusPageChart from '$lib/components/ui/status/status-page-chart.svelte';
+	import StatusPageCard from '$lib/components/ui/status/status-page-card.svelte';
 
 	export let data;
 	let loadingStatusPage = true;
@@ -54,39 +51,11 @@
 				<Skeleton class="mb-4 h-40 w-full" />
 				<Skeleton class="h-40 w-full" />
 			{:else}
-				{#each statusPageItems as statusPageItem}
-					{@const statuses = statusPageItem.statuses}
-					<Card.Root class="mb-4 w-full px-4 py-4 sm:px-8">
-						<div class="mb-3">
-							<div class="flex items-center justify-between">
-								<div class="flex items-center">
-									{#if statusPageItem.statuses.length > 0}
-										<StatusBadge status={statuses[statuses.length - 1].status} />
-									{/if}
-									<h2 class="ml-4 text-lg font-medium">
-										{statusPageItem.name}
-									</h2>
-								</div>
-								<a href={statusPageItem.url} target="_blank" class="text-xs text-muted-foreground">
-									{statusPageItem.url}
-								</a>
-							</div>
-						</div>
-						<div class="flex flex-col items-center justify-between gap-4 sm:flex-row sm:gap-8">
-							<div class="grid w-full grid-cols-2 gap-2 divide-x rounded-lg border sm:w-1/2">
-								<StatusAvailability {statuses} />
-								<StatusPerformance {statuses} />
-							</div>
-							<div class="w-full sm:w-1/2">
-								{#if statuses.length > 0}
-									<StatusPageChart {statuses} />
-								{:else}
-									<p class="text-muted-foreground">No data available</p>
-								{/if}
-							</div>
-						</div>
-					</Card.Root>
-				{/each}
+				<div class="space-y-4">
+					{#each statusPageItems as statusPageItem}
+						<StatusPageCard bind:statusPageItem />
+					{/each}
+				</div>
 			{/if}
 		</section>
 	</main>
