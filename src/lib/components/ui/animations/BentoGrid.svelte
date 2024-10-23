bc7fc5cba4aeca1b945966a97031c9f8512397bc
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-23 12:17:15 +0530

    chore: refactor bento items

--- src/lib/components/ui/animations/BentoGrid.svelte
+++ src/lib/components/ui/animations/BentoGrid.svelte
@@ -6,7 +6,7 @@
 
 <div
 	class={cn(
-		'grid grid-cols-1  gap-4 sm:grid-cols-1 md:auto-rows-[15.4rem] md:grid-cols-1 lg:grid-cols-3 ',
+		'grid grid-cols-1 gap-4 sm:grid-cols-1 md:auto-rows-[15.4rem] md:grid-cols-2 lg:grid-cols-3',
 		className
 	)}
 >
--- src/lib/components/ui/animations/BentoGridItem.svelte
+++ src/lib/components/ui/animations/BentoGridItem.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
 
+	const baseClasses = {
+		layout: 'row-span-1 flex w-full flex-col justify-between space-y-4',
+		appearance: 'rounded-xl border border-transparent bg-white px-6 py-4',
+		interactions: 'group/bento shadow-input transition duration-200 hover:shadow-xl',
+		darkMode: 'dark:border-white/[0.2] dark:bg-black dark:shadow-none'
+	};
+
 	export let className: string | undefined = undefined;
 </script>
 
-<div
-	class={cn(
-		'group/bento row-span-1 flex w-full flex-col justify-between space-y-4 rounded-xl border border-transparent bg-white px-6 py-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none',
-		className
-	)}
->
+<div class={cn(Object.values(baseClasses).join(' '), className)}>
 	<slot />
 </div>
