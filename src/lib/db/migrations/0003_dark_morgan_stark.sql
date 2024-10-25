1e07dac2fb82c60c6e2e570d64911dbfefe784e4
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-25 12:30:15 +0530

    feat: update migrations

--- src/lib/db/migrations/0003_dark_morgan_stark.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `users` ADD `pro` integer DEFAULT false;
--- src/lib/db/migrations/0007_massive_smiling_tiger.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `websites` ADD `user_id` text NOT NULL REFERENCES users(id);--> statement-breakpoint
-ALTER TABLE `websites` DROP COLUMN `status`;
